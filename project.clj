73b7c4ab5e3309a1d5ef108c9e4f46c6b30838d7
Simão Mata <me@simao.io>
2015-01-12 17:09:59 +0100

    start work on tailer

--- project.clj
+++ project.clj
@@ -11,6 +11,7 @@
                  [riemann-clojure-client "0.2.11"]
                  [ch.qos.logback/logback-classic "1.1.1"]
                  [org.clojure/tools.cli "0.3.1"]
+                 [commons-io/commons-io "2.4"]
                  ]
   :main ^:skip-aot riemann-mysql.core
   :target-path "target/%s"
---
+++ src/riemann_mysql/file_checks.clj
@@ -0,0 +1,6 @@
+(ns riemann-mysql.file_checks
+  (:require [riemann-mysql.run_checks :as run_checks]
+            ))
+
+
+(defn wat ())
